4a6a3b9943a20172d26b616ab9a649e50c53c407
gapry <gapry@protonmail.com>
2020-01-04 07:53:17 +0800

    [vector] create a namespace beta

---
+++ 01-vector/inc/beta.hpp
@@ -0,0 +1,12 @@
+#ifndef __NAMESPACES_HPP__
+#define __NAMESPACES_HPP__
+
+namespace beta {
+class noncopyable;
+class test_container;
+
+template <typename T>
+class vector;
+}; // namespace beta
+
+#endif
